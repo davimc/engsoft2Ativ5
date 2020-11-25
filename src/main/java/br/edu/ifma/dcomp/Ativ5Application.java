060f8aaddf9c47798421670af9e832bbc53b9456
davimc <davimatosc@hotmail.com>
2020-11-25 00:56:41 -0300

    first commit

---
+++ src/main/java/br/edu/ifma/dcomp/Ativ5Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifma.dcomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ativ5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ativ5Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifma/dcomp/Ativ5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifma.dcomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ativ5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
