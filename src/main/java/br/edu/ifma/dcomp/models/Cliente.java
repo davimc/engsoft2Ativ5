5c09a8047dd38b9895b8eafe227b6f3727975160
davimc <davimatosc@hotmail.com>
2020-11-26 14:03:02 -0300

    implementando o validations para controlar as entradas diretamente no modelo

--- src/main/java/br/edu/ifma/dcomp/models/Cliente.java
+++ src/main/java/br/edu/ifma/dcomp/models/Cliente.java
@@ -4,14 +4,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotEmpty(message="O nome deve ser preenchido")
     private String nome;
+    @NotEmpty(message="O endereco deve ser preenchido")
     private String endereco;
+    @NotEmpty(message="O telefone deve ser preenchido")
     private String telefone;
 
     public Cliente() {
